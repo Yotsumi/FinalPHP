4df898faf5fd0c5ad4747da2ec767582bf9ca1c7
php_ubuntu <antocapa98@gmail.com>
2019-11-22 04:06:04 -0800

    WIP

--- config/container.php
+++ config/container.php
@@ -1,10 +1,18 @@
 <?php
+
 use League\Plates\Engine;
 use Psr\Container\ContainerInterface;
+use SimpleMVC\Helper;
+use PDO;
+
+$pdoConfig = require 'userPdo.php';
 
 return [
     'view_path' => 'src/View',
     Engine::class => function(ContainerInterface $c) {
         return new Engine($c->get('view_path'));
+    },
+    'public_db_manager' => function () use ($pdoConfig){
+        return new Helper\Test(new PDO($pdoConfig['public']['dsn'], $pdoConfig['public']['user'], $pdoConfig['public']['password']));
     }
 ];
---
+++ config/userPdo.php
@@ -0,0 +1,9 @@
+<?php
+
+$dsn = 'mysql:dbname=testdb;host=127.0.0.1';
+
+return [
+
+    'public' => ['dsn' => $dsn, 'user' => 'test', 'password' => 'test']
+
+];
