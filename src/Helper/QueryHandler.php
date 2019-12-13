b79750dba127c7cb57dc86da6bfdda267f670039
giuseppe-velocci <runnermean@gmail.com>
2019-12-13 21:40:55 +0100

    fix-QueryHandler

--- src/Helper/QueryHandler.php
+++ src/Helper/QueryHandler.php
@@ -11,6 +11,20 @@ class QueryHandler {
         $this->pdo = $pdo;
     }
 
+    protected function selectQueries(string $query, array $args) :?array{
+        try
+        {
+            $sth = $this->pdo->prepare($query);
+            $sth->execute($args);
+            $sth->setFetchMode(PDO::FETCH_CLASS, Utente::class);
+            $utente = $sth->fetchAll();
+            return $utente;
+        }catch(Exception $e){
+            //printf("Errore: %s\n", $e->getMessage());
+            return null;
+        }
+    }
+
     protected function insertQueries(string $query, array $args){
         try
         {
